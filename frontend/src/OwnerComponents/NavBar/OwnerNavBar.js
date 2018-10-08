8648bf4902f18da03d12df6b9f5e94e588a3ccf9
darshilpk3 <darshilpk3@gmail.com>
2018-10-08 13:34:35 -0700

    revertedback

--- frontend/src/OwnerComponents/NavBar/OwnerNavBar.js
+++ frontend/src/OwnerComponents/NavBar/OwnerNavBar.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react'
 // import 'C:/Users/darsh/Desktop/CMPE 273/HomeAway/frontend/src/App.css'
 import '../../App.css'
 import cookie from 'react-cookies'
-import { Redirect } from 'react-router'
+import {Redirect} from 'react-router'
 
 class OwnerNavBar extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            logininfo: null,
-            userinfo: this.props.user
+            logininfo: "Login",
+            userinfo:this.props.user
         }
         this.loginCheck = this.loginCheck.bind(this)
         this.handleLogout = this.handleLogout.bind(this)
@@ -25,21 +25,21 @@ class OwnerNavBar extends Component {
             })
         }
     }
-
+    
     handleLogout = (e) => {
         console.log(cookie.load('loginuser'))
-        cookie.remove('ownerlogin', {
-            path: "/"
+        cookie.remove('ownerlogin',{
+            path:"/"
         });
-        cookie.remove('owneremail', {
-            path: "/"
+        cookie.remove('owneremail',{
+            path:"/"
         });
         this.setState({
-            logininfo: true
+            logininfo:"Logged out"
         })
     }
 
-    componentWillMount() {
+    componentWillMount(){
         this.loginCheck()
     }
     render() {
@@ -56,10 +56,12 @@ class OwnerNavBar extends Component {
         }
         else {
             navVar = <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">Welcome {cookie.load("loginemail") && cookie.load("loginemail")}</a>
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">Welcome {cookie.load('owneremail') && cookie.load('owneremail')}</a>
                 <div class="dropdown-menu">
-                    <a class="dropdown-item" href={"/traveller/edit/" + cookie.load("loginuser")}>Account Information</a>
-                    <button class="dropdown-item" onClick={this.handleLogout}>Logout</button>
+                <a class="dropdown-item" href={"/owner/edit/"+cookie.load("ownerlogin")}>Profile Settings</a>
+                <a class="dropdown-item" href={"/owner/property/show"}>Property Details</a>
+                <a class="dropdown-item" href={"/owner/property/add"}>Add new Property</a>
+                <button class="dropdown-item" onClick={this.handleLogout}>Sign out</button>
                 </div>
             </li>
         }
@@ -74,22 +76,22 @@ class OwnerNavBar extends Component {
                         <ul class="navbar-nav ml-auto">
                             {navVar}
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
-                                    Help
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
+                                        Help
                         </a>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="#">Visit Help Center</a>
-                                    <div class="dropdown-divider"></div>
-                                    <p class="dropdown-header">Travelers</p>
-                                    <a class="dropdown-item" href="#">How it works</a>
-                                    <a class="dropdown-item" href="#">Security Center</a>
-                                    <div class="dropdown-divider"></div>
-                                    <p class="dropdown-header">Homeowners</p>
-                                    <a class="dropdown-item" href="#">How it works</a>
-                                    <a class="dropdown-item" href="#">Community</a>
-                                    <a class="dropdown-item" href="#">Discovery Hub</a>
-                                </div>
-                            </li>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="#">Visit Help Center</a>
+                                        <div class="dropdown-divider"></div>
+                                        <p class="dropdown-header">Travelers</p>
+                                        <a class="dropdown-item" href="#">How it works</a>
+                                        <a class="dropdown-item" href="#">Security Center</a>
+                                        <div class="dropdown-divider"></div>
+                                        <p class="dropdown-header">Homeowners</p>
+                                        <a class="dropdown-item" href="#">How it works</a>
+                                        <a class="dropdown-item" href="#">Community</a>
+                                        <a class="dropdown-item" href="#">Discovery Hub</a>
+                                    </div>
+                                </li>
                             <li class="nav-item dropdown navbar-brand">
                                 <img class="navbar-brand" src="../../birdhouse-bceheader-white.svg" />
                             </li>
