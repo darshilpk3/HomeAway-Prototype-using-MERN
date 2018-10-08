19c19d4af92aee46593af4505a49de32940526eb
darshilpk3 <darshilpk3@gmail.com>
2018-10-08 13:15:20 -0700

    Decoration

--- frontend/src/OwnerComponents/NavBar/OwnerNavBar.js
+++ frontend/src/OwnerComponents/NavBar/OwnerNavBar.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react'
 // import 'C:/Users/darsh/Desktop/CMPE 273/HomeAway/frontend/src/App.css'
 import '../../App.css'
 import cookie from 'react-cookies'
-import {Redirect} from 'react-router'
+import { Redirect } from 'react-router'
 
 class OwnerNavBar extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            logininfo: "Login",
-            userinfo:this.props.user
+            logininfo: null,
+            userinfo: this.props.user
         }
         this.loginCheck = this.loginCheck.bind(this)
         this.handleLogout = this.handleLogout.bind(this)
@@ -25,37 +25,41 @@ class OwnerNavBar extends Component {
             })
         }
     }
-    
+
     handleLogout = (e) => {
         console.log(cookie.load('loginuser'))
-        cookie.remove('ownerlogin',{
-            path:"/"
+        cookie.remove('ownerlogin', {
+            path: "/"
         });
-        cookie.remove('owneremail',{
-            path:"/"
+        cookie.remove('owneremail', {
+            path: "/"
         });
         this.setState({
-            logininfo:"Logged out"
+            logininfo: true
         })
     }
 
-    componentWillMount(){
+    componentWillMount() {
         this.loginCheck()
     }
     render() {
 
         let navVar = null;
         if (!cookie.load("ownerlogin")) {
-            <Redirect to="/owner/login"/>
-        }
-        else{
             navVar = <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">Welcome {cookie.load('owneremail') && cookie.load('owneremail')}</a>
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">Login</a>
                 <div class="dropdown-menu">
-                    <a class="dropdown-item" href={"/owner/edit/"+cookie.load("ownerlogin")}>Profile Settings</a>
-                    <a class="dropdown-item" href={"/owner/property/show"}>Property Details</a>
-                    <a class="dropdown-item" href={"/owner/property/add"}>Add new Property</a>
-                    <button class="dropdown-item" onClick={this.handleLogout}>Sign out</button>
+                    <a class="dropdown-item" href="/traveller/login">Traveller Login</a>
+                    <a class="dropdown-item" href="/owner/login">Owner Login</a>
+                </div>
+            </li>
+        }
+        else {
+            navVar = <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">Welcome {cookie.load("loginemail") && cookie.load("loginemail")}</a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href={"/traveller/edit/" + cookie.load("loginuser")}>Account Information</a>
+                    <button class="dropdown-item" onClick={this.handleLogout}>Logout</button>
                 </div>
             </li>
         }
@@ -69,6 +73,23 @@ class OwnerNavBar extends Component {
                     <div class="collapse navbar-collapse" id="navbarResponsive">
                         <ul class="navbar-nav ml-auto">
                             {navVar}
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
+                                    Help
+                        </a>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="#">Visit Help Center</a>
+                                    <div class="dropdown-divider"></div>
+                                    <p class="dropdown-header">Travelers</p>
+                                    <a class="dropdown-item" href="#">How it works</a>
+                                    <a class="dropdown-item" href="#">Security Center</a>
+                                    <div class="dropdown-divider"></div>
+                                    <p class="dropdown-header">Homeowners</p>
+                                    <a class="dropdown-item" href="#">How it works</a>
+                                    <a class="dropdown-item" href="#">Community</a>
+                                    <a class="dropdown-item" href="#">Discovery Hub</a>
+                                </div>
+                            </li>
                             <li class="nav-item dropdown navbar-brand">
                                 <img class="navbar-brand" src="../../birdhouse-bceheader-white.svg" />
                             </li>
--- frontend/src/TravellerComponents/SignUpPage/SignUpPage.js
+++ frontend/src/TravellerComponents/SignUpPage/SignUpPage.js
@@ -1,57 +1,94 @@
 import React, { Component } from 'react'
 import Navbar from '../NavBar/NavBar'
-import {Redirect} from 'react-router'
+import { Redirect } from 'react-router'
 import '../../App.css'
 import axios from 'axios'
+import { FormErrors } from '../../FormErrors';
 
 class SignUpPage extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
-            firstname:"",
-            lastname:"",
-            email:"",
-            password:"",
-            redirect:false,
-            message:""
+            firstname: "",
+            lastname: "",
+            email: "",
+            password: "",
+            redirect: false,
+            message: "",
+            formErrors: { email: "", password: "", firstname: "", lastname: "" },
+            emailValid: false,
+            passwordValid: false,
+            firstnameValid: "",
+            lastnameValid: "",
+            formValid: false
         }
-        this.handleFirstName = this.handleFirstName.bind(this);
-        this.handleLastName = this.handleLastName.bind(this);
-        this.handleEmail = this.handleEmail.bind(this);
-        this.handlePassword = this.handlePassword.bind(this);
+        this.handleChange = this.handleChange.bind(this);
         this.signUp = this.signUp.bind(this);
     }
 
-    handleFirstName = (e) => {
+    handleChange = (e) => {
+        const name = e.target.name
+        const value = e.target.value
+        console.log(name," - ",value)
         this.setState({
-            firstname:e.target.value
-        });
+            [name]: value,
+        }, () => {
+            this.validateField(name, value)
+        })
     }
 
-    handleLastName = (e) => {
+
+    validateField(fieldName, value) {
+        let fieldValidationErrors = this.state.formErrors;
+        let emailValid = this.state.emailValid;
+        let passwordValid = this.state.passwordValid;
+        let firstnameValid = this.state.firstnameValid;
+        let lastnameValid = this.state.lastnameValid
+
+        switch (fieldName) {
+            case 'email':
+                //emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+                emailValid = value.length >= 1;
+                fieldValidationErrors.email = emailValid ? '' : ' is invalid';
+                break;
+            case 'firstname':
+                //emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+                firstnameValid = value.length >= 1;
+                fieldValidationErrors.firstname = firstnameValid ? '' : ' is invalid';
+                break;
+            case 'lastname':
+                //emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+                lastnameValid = value.length >= 1;
+                fieldValidationErrors.lastname = lastnameValid ? '' : ' is invalid';
+                break;
+            case 'password':
+                passwordValid = value.length >= 1;
+                fieldValidationErrors.password = passwordValid ? '' : ' is too short';
+                break;
+            default:
+                break;
+        }
         this.setState({
-            lastname:e.target.value
-        });
+            formErrors: fieldValidationErrors,
+            emailValid: emailValid,
+            passwordValid: passwordValid,
+            firstnameValid:firstnameValid,
+            lastnameValid:lastnameValid
+        }, this.validateForm);
     }
 
-    handleEmail = (e) => {
-        this.setState({
-            email:e.target.value
-        });
+    validateForm() {
+        this.setState({ formValid: this.state.emailValid && this.state.passwordValid && this.state.firstnameValid && this.state.lastnameValid});
     }
 
-    handlePassword = (e) => {
-        this.setState({
-            password:e.target.value
-        });
-    }
+
 
     signUp = (e) => {
-        
-        var headers = new Headers(); 
-        e.preventDefault(); 
-        axios.defaults.withCredentials = true; 
+
+        var headers = new Headers();
+        e.preventDefault();
+        axios.defaults.withCredentials = true;
 
         console.log("Trying to sign up");
         const data = {
@@ -61,33 +98,33 @@ class SignUpPage extends Component {
             password: this.state.password
         }
 
-        axios.post("http://localhost:3001/travel/signup",data)
-            .then(response=>{
+        axios.post("http://localhost:3001/travel/signup", data)
+            .then(response => {
                 console.log(response.data);
 
-                if(response.data === "Could not add the user"){
+                if (response.data === "Could not add the user") {
                     console.log("couldnt add")
                     this.setState({
-                        message:"Account creation unsuccessfull"
+                        message: "Account creation unsuccessfull"
                     })
                 }
-                else{
+                else {
                     console.log("added")
                     this.setState({
-                        redirect:true
+                        redirect: true
                     })
                 }
-        })
+            })
     }
 
     render() {
         let renderRedirect = null;
-        if(this.state.redirect == true){
+        if (this.state.redirect == true) {
             console.log("should redirect")
-            renderRedirect = <Redirect to="/traveller/login"/>
-        } 
+            renderRedirect = <Redirect to="/traveller/login" />
+        }
         return (
-            
+
             <div>
                 {renderRedirect}
                 <div>
@@ -102,6 +139,7 @@ class SignUpPage extends Component {
                             </div>
                             <div class="col-md-4 offset-md-4 text-align-center">
                                 <footer class="form-footer">Already have an account? <a href="/login">Log in</a></footer>
+                                <p class="form-footer text-danger"><FormErrors formErrors={this.state.formErrors} /></p>
                                 <footer class="form-footer text-danger">{this.state.message}</footer>
                             </div>
                         </div>
@@ -111,15 +149,15 @@ class SignUpPage extends Component {
                         <fieldset>
                             <form class="form-group">
                                 <div class="flex-it">
-                                    <input class="form-control" type="text" placeholder="First Name" onChange={this.handleFirstName} id="firstname" required/>
-                                    <input class="form-control" type="text" onChange={this.handleLastName} placeholder="Last Name" id="lastname" required/>
+                                    <input class="form-control" type="text" placeholder="First Name" onChange={this.handleChange} value={this.state.firstname} name="firstname" required />
+                                    <input class="form-control" type="text" onChange={this.handleChange} value={this.state.lastname} placeholder="Last Name" name="lastname" required />
                                 </div>
                                 <div class="clearfix"></div>
-                                <input class="form-control" type="email" onChange={this.handleEmail} placeholder="Email Address" id="password" required/>
+                                <input class="form-control" type="email" onChange={this.handleChange} value={this.state.email} placeholder="Email Address" name="email" required />
                                 <div class="clearfix"></div>
-                                <input class="form-control" type="password" onChange={this.handlePassword} placeholder="Password" id="password" required/>
+                                <input class="form-control" type="password" onChange={this.handleChange} value={this.state.password} placeholder="Password" name="password" required />
                                 <div class="clearfix"></div>
-                                <button class="form-control-login btn btn-warning" value="Sign me up" onClick={this.signUp}>Sign me up</button>
+                                <button class="form-control-login btn btn-warning" value="Sign me up" disabled={!this.state.formValid} onClick={this.signUp}>Sign me up</button>
                                 <div class="clearfix"></div>
                                 <footer class="form-footer">By creating an account you are accepting our <a href="#">Terms and Conditions and Privacy Policy</a>.</footer>
                             </form>
